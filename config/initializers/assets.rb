373dc8d2b419ba95c2ca62efecd60ddbe8cb1a67
NGUYEN NGOC TRUNG <nguyen.ngoc.trung@framgia.com>
2018-03-07 10:02:55 +0700

    Custom fonts added as base64

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( glanz/images/* )
 Rails.application.config.assets.precompile += %w( glanz/fonts/* )
+Rails.application.config.assets.precompile += %w( pdf.css.scss )
